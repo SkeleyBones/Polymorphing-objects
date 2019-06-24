d0420f4c26073a7eb6f6993c36b7369b155c45f7
Aiden <owlcapone0@gmail.com>
2019-06-24 18:02:14 +1200

    Displaying use of console

---
+++ OOJ.js
@@ -0,0 +1,32 @@
+// create a function that console logs any argument given
+function display (content){
+console.log(content);
+}
+
+// create a function that takes a name(string) as a parameter
+// and returns "hi my name is (perameter given)"
+function name (name){
+    return "my name is " + name + " .";
+    }
+    
+    // Create three functions that describe the powers of a ninja
+    // you would like to create
+
+
+function power1 (power1N){
+    return "Using " + power1N + " .";
+    }
+
+function power2 (power2N){
+    return "Using " + power2N + " .";
+}
+
+
+function power3 (power3N){
+    return power3N + " nothin personal kid.";
+}
+
+    display(power1("Quad dash"));
+    display(power2("ranged guardbreak"));
+    display(power3("teleports behind you."));
+    display(name("Shinobi"));
